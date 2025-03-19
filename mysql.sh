f79fe0738adf2286ce6bcda671cb8606b1cb659a
vara1108 <varaprasanna0811@gmail.com>
2025-03-19 21:47:21 +0530

    expense-shell

--- mysql.sh
+++ mysql.sh
@@ -42,7 +42,7 @@ VALIDATE $? "Enabling MySQL Server"
 systemctl start mysqld &>>$LOG_FILE_NAME
 VALIDATE $? "Starting MySQL Server"
 
-mysql -h mysql.daws82s.in -u root -pExpenseApp@1 -e 'show databases;'
+mysql -h mysql.daws82s.in -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then
